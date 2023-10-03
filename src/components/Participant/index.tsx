3030aa8c180c87f53785c64a6379091feea763c0
João Pedro Ferreira <joaof082@gmail.com>
2023-10-03 15:11:46 -0300

    feat: adding function of delete user and types of typescript
    Signed-off-by: João Pedro Ferreira <joaof082@gmail.com>

--- src/components/Participant/index.tsx
+++ src/components/Participant/index.tsx
@@ -2,12 +2,17 @@ import { Text, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./styles";
 
-export function Participant() {
+type Props = {
+  name: string;
+  onRemove: () => void;
+};
+
+export function Participant({ name, onRemove }: Props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>Rodrigo Gonçalves</Text>
+      <Text style={styles.name}>{name}</Text>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={onRemove}>
         <Text style={styles.buttonText}>-</Text>
       </TouchableOpacity>
     </View>
